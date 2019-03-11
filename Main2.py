9126778f1fe647f02cc6c5d846ea11e5579ee003
J20202 <42816568+J20202@users.noreply.github.com>
2019-03-11 13:55:31 -0700

    Update Main2.py

--- Main2.py
+++ Main2.py
@@ -9,8 +9,10 @@ done = False
 # Game Variables
 # 1 = Red | 2 = Yellow
 turn = 1
+col = 0
+
 #time
-time = 500
+time = 1000
 
 # Board
 # 0 = Empty, 1 = red, 2 = yellow
@@ -46,32 +48,58 @@ def drawHitBox():
     global turn
     global time
 
-    #increases time
-    time += 1
-    print(time)
-
-    # Sets mPos (Mouse Position) to current xy of mouse.  Stored as (x,y)
     mPos = pygame.mouse.get_pos()
-    if time > 500:
+    if time > 1000:
         if pygame.mouse.get_pressed()[0]:
             for a in range(0,len(board),1):
                 for i in range(0, len(board[a]), 1):
                     hitBox = pygame.Rect(75 + 100 * i, 75 + 75 * a, 50, 50)
                     if hitBox.collidepoint(mPos):
                         board[a][i] = turn
-            changeTurn()
+
+
+
+
+                        changeTurn()
+                        time = 0
+
+
+'''def findColumn():
+    global mPos
+    global col
+
+    if mPos[1] > 75 and mPos[1] <125:
+        col = 1
+        print col
+    elif mPos[1] > 175 and mPost[1] < 225'''
+
+
+def checkForWin():
+    pass
+
+def gravity():
+    global col
+
+    col = len(board)
 
 def changeTurn():
     global turn
-    global time
-
-    time = 0
 
     if turn == 1: #RED
         turn = 2
     else:
         turn = 1
 
+def incTime():
+    global time
+
+    time += 1
+
+def test():
+    global mPos
+
+    if pygame.mouse.get_pressed()[2]:
+        print mPos
 
 
 
@@ -84,5 +112,10 @@ while not done:
 
     drawGame()
     drawHitBox()
+    incTime()
+    #findColumn()
+
+    test()
+    gravity()
 
     pygame.display.flip()
